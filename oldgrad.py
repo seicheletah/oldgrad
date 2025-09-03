65b3f283c41b2d5e6f547134554515b5c1a17a1d
Sannik Biswas <sannikbiswas@gmail.com>
2025-09-04 01:53:49 +0530

    added shell context processor

--- oldgrad.py
+++ oldgrad.py
@@ -1,1 +1,8 @@
-from app import oldgrad
+from app import oldgrad, oldgrad_db
+import sqlalchemy as sql
+import sqlalchemy.orm as sqlorm
+from app.models import User, EventPost, JobPost, Donations
+
+@oldgrad.shell_context_processor
+def make_shell_context():
+    return {'sql':sql, 'sqlorm':sqlorm, 'oldgrad_db':oldgrad_db, 'User':User, 'EventPost':EventPost, 'JobPost':JobPost, 'Donations':Donations}
